a4ba42c98e90b6f3957f6ca51b9146d12b1da5ed
Jeff Yates <jeff@khanacademy.org>
2024-01-23 18:00:25 -0600

    Tags formatting

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -44,13 +44,17 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const Tag = styled.li`
+  display: inline;
+  margin-right: 8px;
   padding: 4px 8px;
+  line-height: 1.5;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
